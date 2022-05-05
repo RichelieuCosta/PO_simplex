e3e597e2d604f8f932fb79bd8dfb7106a624731f
Richelieu Costa <richelieucosta@gmail.com>
2022-05-05 16:16:47 -0300

    Rearranjando matrizes com os coeficientes básicos e não básicos

--- import_matriz.py
+++ import_matriz.py
@@ -1,76 +1,81 @@
 from ctypes import c_bool
 from imp import is_builtin
 import numpy as np
-arq = open('arquivo.txt', 'r')  #abre o arquivo
-texto = []  #declaro um vetor
-matriz = [] #declaro um segundo vetor
-texto = arq.readlines() #quebra as linhas do arquivo em vetores 
-print("vetor texto -> ",texto) #aqui eu mostro
+arq = open('arquivo.txt', 'r')  # abre o arquivo
+texto = []  # declaro um vetor
+matriz = []  # declaro um segundo vetor
+texto = arq.readlines()  # quebra as linhas do arquivo em vetores
+print("vetor texto -> ", texto)  # aqui eu mostro
 print("")
 
-for i in range(len(texto)):          #esse for percorre a posições dp vetor texto
-    matriz.append(np.array(texto[i].split()))  #aqui eu quebro nos espasos das palavras
+for i in range(len(texto)):  # esse for percorre a posições dp vetor texto
+    # aqui eu quebro nos espasos das palavras
+    matriz.append(np.array(texto[i].split()))
 
-print("vetor matriz -> ",matriz) #mostra o vertor com um conjunto de vetores
+print("vetor matriz -> ", matriz)  # mostra o vertor com um conjunto de vetores
 print("")
 
-for i in range(len(texto)):          #mostra quedrando em linhas
-    print(matriz[i])  
+for i in range(len(texto)):  # mostra quedrando em linhas
+    print(matriz[i])
 
 
-arq.close() #comando para fechar o arquivo
+arq.close()  # comando para fechar o arquivo
 
-def obter_particoes(A,b):
+
+def obter_particoes(A, b):
 
     return I_b, I_n
 
-def primal_simplex(A,x,b,c):
+
+def primal_simplex(A, x, b, c):
 
     return x_x_b
-# variavéis que precisarei: 
+# variavéis que precisarei:
 
-A = [] # matrix_Ax
+
+A = []  # matrix_Ax
 A = matriz[:][:-1]
 b = []
 b = matriz[:][len(matriz[1])-1]
 
-I_b = [] #indice das variáveis que estão na partição básica
-I_n = [] #indice das variáveis que estão na partição não básica
+I_b = []  # indice das variáveis que estão na partição básica
+I_n = []  # indice das variáveis que estão na partição não básica
 A_inv = []
 
-I_b, I_n=obter_particoes(A, b)
+I_b, I_n = obter_particoes(A, b)
 
 
-B = [] #Matrix_Básica
+B = []  # Matrix_Básica
 print(B.T)
 
-m # numero de equações
-n # número de variáveis
-#resolvendo sistema de equações lineares:
-#Solve the system of equations x0 + 2 * x1 = 1 and 3 * x0 + 5 * x1 = 2:
+m  # numero de equações
+n  # número de variáveis
+# resolvendo sistema de equações lineares:
+# Solve the system of equations x0 + 2 * x1 = 1 and 3 * x0 + 5 * x1 = 2:
 #a = np.array([[1, 2], [3, 5]])
 #b = np.array([1, 2])
 #x = np.linalg.solve(a, b)
-#x
+# x
 
-N = [] # matrix_NãoBásica
-a_n = [] # a_n[] são as colunas de N
+N = []  # matrix_NãoBásica
+a_n = []  # a_n[] são as colunas de N
 x = []
-x_x_b = [] # solução_avaliada
-x_x_b = np.multiply(np.linalg.inv(B),b)
+x_x_b = []  # solução_avaliada
+x_x_b = np.multiply(np.linalg.inv(B), b)
 if x_x_b >= 0:
     print("solução básica viável")
-Lambda = [] # vetor multiplicador simplex
+Lambda = []  # vetor multiplicador simplex
 
-c = [] # vetor de custos
+c = []  # vetor de custos
 c_b = []  # vetor de coeficientes das variáveis básicas
-c_n = [] # vetor de coeficientes das variáveis não básicas
-c_xapeu_n = c_n[:] - np.multiply(Lambda.T,a_n[:]) # custos relativos não básicos
-y = [] # direção do simplex
-y[:] = np.multiply(np.linalg.inv(B),N[:]) # Errado. quero pegar as colunas de N
-c_xapeu_k = [] # teste de otimalidade nas não básicas
- min{c_n}
+c_n = []  # vetor de coeficientes das variáveis não básicas
+# custos relativos não básicos
+c_xapeu_n = c_n[:] - np.multiply(Lambda.T, a_n[:])
+y = []  # direção do simplex
+# Errado. quero pegar as colunas de N
+y[:] = np.multiply(np.linalg.inv(B), N[:])
+c_xapeu_k = []  # teste de otimalidade nas não básicas
+min{c_n}
 E_xapeu = []
 E_xapeu[:] = x_x_b[:]/y[:]
 # np.linalg.inv(B)
-
--- teste_particoes.py
+++ teste_particoes.py
@@ -1,29 +1,162 @@
 import numpy as np
 
-def obter_particoes(A,b):
+
+def atualizeBN(A, I_b, I_n):
+    B = []
+    N = []
+
+    for linhaA in A:
+        linhaB = []
+        linhaN = []
+        for j in I_b:
+            print(j)
+            linhaB.append(linhaA[j-1])
+            print(linhaB)
+        B.append(linhaB)
+        for j in I_n:
+            print(j)
+            linhaN.append(linhaA[j-1])
+            print(linhaN)
+        N.append(linhaN)
+
+    return B, N
+
+
+def obter_particoes_iniciais(A, b):
+
     len(A)
-    I_b=np.zeros((len(A)), dtype=int)
-    I_n=np.zeros((len(A[1])-len(A)), dtype=int)
+    I_b = np.zeros((len(A)), dtype=int)
+    I_n = np.zeros((len(A[1])-len(A)), dtype=int)
 
+    x_x_b = []  # solução_avaliada
+    x_x_b = np.multiply(np.linalg.inv(B), b)
 
-    
     return I_b, I_n
 
-A= [[1, 1, 1, 0, 0],
-    [1, -1, 0, 1, 0],
-    [3, 1, 0, 0, -1]]
-    
+
+A = [[1, 1, 1, 0, 0],
+     [1, -1, 0, 1, 0],
+     [3, 1, 0, 0, -1]]
+
 for l in A:
     print(l)
 
-b= [6, 4, 3]
+b = [6, 4, 3]
+I_b = []  # indice das variáveis que estão na partição básica
+I_n = []  # indice das variáveis que estão na partição não básica
+# A_inv = []
+print("ola")
+
+I_b = [1, 2, 5]
+I_n = [3, 4]
+#I_b, I_n = obter_particoes_iniciais(A, b)
+print("partições básicas:", I_b)
+print("partições não básicas:", I_n)
+
+B, N = atualizeBN(A, I_b, I_n)
+print("Saiu da função de atualização de BN")
+
+print("matriz Básica:")
+for l in B:
+    print(l)
+
+print("matriz não básica")
+for l in N:
+    print(l)
+
+x_x_b = []  # solução_avaliada
+x_x_b = np.multiply(np.linalg.inv(B), b)
+
+
+""".git\A = [] # matrix_Ax
+A = matriz[:][:-1]
+b = []
+b = matriz[:][len(matriz[1])-1]
 
 I_b = [] #indice das variáveis que estão na partição básica
 I_n = [] #indice das variáveis que estão na partição não básica
-# A_inv = []
-print("ola")
+A_inv = []
+
 I_b, I_n=obter_particoes(A, b)
 
-print(I_b)
-print(I_n)
 
+B = [] #Matrix_Básica
+print(B.T)
+
+m # numero de equações
+n # número de variáveis
+#resolvendo sistema de equações lineares:
+#Solve the system of equations x0 + 2 * x1 = 1 and 3 * x0 + 5 * x1 = 2:
+#a = np.array([[1, 2], [3, 5]])
+#b = np.array([1, 2])
+#x = np.linalg.solve(a, b)
+#x
+
+N = [] # matrix_NãoBásica
+a_n = [] # a_n[] são as colunas de N
+x = []
+
+if x_x_b >= 0:
+    print("solução básica viável")
+Lambda = [] # vetor multiplicador simplex
+
+c = [] # vetor de custos
+c_b = []  # vetor de coeficientes das variáveis básicas
+c_n = [] # vetor de coeficientes das variáveis não básicas
+c_xapeu_n = c_n[:] - np.multiply(Lambda.T,a_n[:]) # custos relativos não básicos
+y = [] # direção do simplex
+y[:] = np.multiply(np.linalg.inv(B),N[:]) # Errado. quero pegar as colunas de N
+c_xapeu_k = [] # teste de otimalidade nas não básicas
+ min{c_n}
+E_xapeu = []
+E_xapeu[:] = x_x_b[:]/y[:]
+# np.linalg.inv(B)
+
+"""
+
+""".git\A = [] # matrix_Ax
+A = matriz[:][:-1]
+b = []
+b = matriz[:][len(matriz[1])-1]
+
+I_b = [] #indice das variáveis que estão na partição básica
+I_n = [] #indice das variáveis que estão na partição não básica
+A_inv = []
+
+I_b, I_n=obter_particoes(A, b)
+
+
+B = [] #Matrix_Básica
+print(B.T)
+
+m # numero de equações
+n # número de variáveis
+#resolvendo sistema de equações lineares:
+#Solve the system of equations x0 + 2 * x1 = 1 and 3 * x0 + 5 * x1 = 2:
+#a = np.array([[1, 2], [3, 5]])
+#b = np.array([1, 2])
+#x = np.linalg.solve(a, b)
+#x
+
+N = [] # matrix_NãoBásica
+a_n = [] # a_n[] são as colunas de N
+x = []
+x_x_b = [] # solução_avaliada
+x_x_b = np.multiply(np.linalg.inv(B),b)
+if x_x_b >= 0:
+    print("solução básica viável")
+Lambda = [] # vetor multiplicador simplex
+
+c = [] # vetor de custos
+c_b = []  # vetor de coeficientes das variáveis básicas
+c_n = [] # vetor de coeficientes das variáveis não básicas
+c_xapeu_n = c_n[:] - np.multiply(Lambda.T,a_n[:]) # custos relativos não básicos
+y = [] # direção do simplex
+y[:] = np.multiply(np.linalg.inv(B),N[:]) # Errado. quero pegar as colunas de N
+c_xapeu_k = [] # teste de otimalidade nas não básicas
+ min{c_n}
+E_xapeu = []
+E_xapeu[:] = x_x_b[:]/y[:]
+# np.linalg.inv(B)
+
+"""
