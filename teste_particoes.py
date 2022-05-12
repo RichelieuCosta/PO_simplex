9c69a6d5ae853bdc28d223afab11effd81ad194b
Richelieu Costa <richelieu.costa@ci.ufpb.br>
2022-05-12 18:04:44 -0300

    obtendo resultado dual a partir do primal e finalizando calculo do dual

--- teste_particoes.py
+++ teste_particoes.py
@@ -4,6 +4,7 @@ from itertools import combinations
 # 1) escrever modelo dual na tela - OK
 # 2) simplex  -  ok
 # 3) determinar valores da solução primal -  ok
+# 3.1) determinar valores da solução dual a partir da primal - 
 # 4) determinar valores da solução dual - pendente de correção, atualização de nomes das variaveis dual e conclusão.
 # 5) determinar ranges do lado direito - pendente.
 
@@ -238,16 +239,17 @@ def primal_simplex(A, b, c_t, I_b, I_n):
     return c_xapeu_n, I_b, I_n, x_x_b
 
 
-def dual_simplex(A, b, c_t):  # , I_b, I_n):
 
-    I_b, I_n = obter_particoes_iniciais(A, b)
+def dual_simplex(A, b, c_t , I_b, I_n):
+
+    #I_b, I_n = obter_particoes_iniciais(A, b)
 
     index_entrar = 0
     index_sair = 0
 
-    # número de variáveis Primal. O numero de restricoes duais ́e igual ao n ́umero de variáveis X do primal
+    # número de variáveis Primal. O numero de restricoes duais ́e igual ao numero de variáveis X do primal
     n = len(A)
-    m = len(A[1])  # numero de equações do primal
+    m = len(A[1])  # numero de equações do primal que é igual ao número de variaveis do dual
     # m
     # n
 
@@ -272,10 +274,11 @@ def dual_simplex(A, b, c_t):  # , I_b, I_n):
     print("B_inv:")
     print(B_inv)
 
-    x_x_b = []  # solução_avaliada
-    x_x_b = B_inv.dot(np.matrix(b).getT())
     print("custos das variáves básicas: ", c_b)
     print("custos das variáves não básicas: ", c_n)
+
+    # Passo 1
+
     lambda_t = np.matrix(c_b).dot(B_inv)
 
     print("valor de lambda: ", lambda_t)
@@ -292,74 +295,90 @@ def dual_simplex(A, b, c_t):  # , I_b, I_n):
 
     y = np.arange(len(A))
 
-    if c_xapeu_n.min() < 0:
-        for k in range(len(I_n)):
-            print("valor de k: ", k)
-            if c_xapeu_n[k] < 0:
-                print("a kaézima variável não básica vai sair na base?")
-                if c_xapeu_n.min() == c_xapeu_n[k]:
-                    print(
-                        "a kaézima variável não básica VAI sair, que é a variável: ", I_n[k])
-                    index_sair = k
-                    for k in range(len(I_n)):
-                        y = B_inv.dot(obter_coluna(N, k))
-                    print("y: ", y)
-                    aux_count = 0
-                    for y_i in y:
-                        if y_i <= 0:
-                            y_i += 1
-                    if aux_count == len(y):
-                        print("não tem solução ótima finita!")
-                        return [0, 0, 0], I_b, I_n, x_x_b
-                    break
+    # Passo 2:
 
-                else:
-                    print("a kaézima variável não básica NÃO vai entrar")
+    # Passo 2.1:
+    
+    x_x_b = []  # solução_avaliada
+    x_x_b = B_inv.dot(np.matrix(b).getT())
+
+    # Passo 2.2:
+
+    print("O x chapeu: ", x_x_b)
+    aux_menor = x_x_b[0]
+    # if x_x_b.min() >= 0:
+    for x_x_b_i in x_x_b:
+        if x_x_b_i < aux_menor:
+            aux_menor = x_x_b_i
+    if aux_menor >= 0:
+        print("Conjunto legal pra iniciar!!", i_b)
+        print("O x chapeu que passou: ")
+        print(x_x_b)
+        for i_n in lista_aux:
+            if i_b.count(i_n) == 0:
+                # print(i_n, " Não está está na partição básica")
+                I_n.append(i_n)
             else:
-                print("acho que deu certo!! será??")
+                I_b.append(i_n)
+
+    eta = np.arange(len(I_n))
+    e_vetor_canonico = np.arange(len(I_n))
+    if aux_menor < 0:
+        # Passo 3
+        for l in range(len(I_n)):
+            print("valor de l: ", l)
+            for l2 in range(len(I_n)):
+                if l == l2:
+                    e_vetor_canonico[l2] = 1
+                else:
+                    e_vetor_canonico[l2] = 0
+            eta[l]=(-1)*(np.matrix(np.linalg.inv(B)).getT().dot(e_vetor_canonico))
+
+        # Passo 4
+        denominador_sigma = np.arange(len(I_n))
+        for l in range(len(I_n)):
+            denominador_sigma[l] = eta[l].dot(obter_coluna(N, j))
+            if denominador_sigma[l] <= 0:
+                print("problema primal inviável. Error!!!")
+                return "error"
+
+        sigma_analise = np.arange(len(I_n))
+        sigma_analise = c_xapeu_n[:]/denominador_sigma[:]
+
+        aux_MIN_sigma_xapeu = sigma_analise[0]
+        index_entrar = 0
+        for i in range(len(sigma_analise)):
+            if sigma_analise[i] < aux_MIN_sigma_xapeu:
+                aux_MIN_sigma_xapeu = sigma_analise[i]
+                index_entrar = i
+
+        print("   'epsilon_xapeu': ", epsilon_xapeu)
+        print("epsilon_xapeu menor: ", epsilon_xapeu[index_sair])
+        print("A variável a sair da base será: ",
+            I_b[index_sair], "de index: ", index_sair)
+
+        aux_troca = I_b[index_sair]
+        I_b[index_sair] = I_n[index_entrar]
+        I_n[index_entrar] = aux_troca
+        return c_xapeu_n, I_b, I_n, x_x_b
+
+            
+
+
     else:
         print("Solução ótima, acabou!")
         return c_xapeu_n, I_b, I_n, x_x_b
 
-    print("não foi dessa vez. Próxima iteração!")
-    print("x_x_b: ", x_x_b)
-    epsilon_xapeu = np.arange(len(y))
-    for i in range(len(I_b)):
-      # for j in range(len(I_b)):
-        # if y[i] > 0:
-        # EESSA MULTIPLICAÇÃO PODE DAR ERRADO !!!!!!!!!!!!
-        epsilon_xapeu[i] = B_t[i][i]*1*(-1)
-    denominador_calc_sigma = []
-    for j in range(len(I_n)):
-        denominador_calc_sigma[j] = epsilon_xapeu.dot(obter_coluna(N, j))
-    aux_MIN_sigma_xapeu = 0
-    index_entrar = 0
-    aux_sigma = c_n[:]/denominador_calc_sigma[:]
-    aux_MIN_sigma_xapeu = aux_sigma[0]
-    index_entrar = 0
-    for i in range(len(I_n)):
-        if aux_sigma[i] < aux_MIN_sigma_xapeu:
-            index_entrar = i
-            aux_MIN_sigma_xapeu = aux_sigma[i]
-        # por falta de prática com python, escolhi essa forma de pegar o menor valor de epsilon_xapeu
-    #aux_MIN_epsilon_xapeu = epsilon_xapeu[i]
-    #index_sair = i
+def obter_solucao_dual_dada_primal(B,c_b): # I_b, c_t
 
-    for i in range(len(y)):
-        if y[i] > 0 and epsilon_xapeu[i] < aux_MIN_epsilon_xapeu:
-            aux_MIN_epsilon_xapeu = epsilon_xapeu[i]
-            index_sair = i
-    # epsilon_xapeu = x_x_b[:]/y[:]
-    print("   'epsilon_xapeu': ", epsilon_xapeu)
-    print("epsilon_xapeu menor: ", epsilon_xapeu[index_sair])
-    print("A variável a sair da base será: ",
-          I_b[index_sair], "de index: ", index_sair)
+    #B_inv.dot(np.matrix(b).getT())
+    B_inv_t = np.matrix(np.linalg.inv(B)).getT()
+    lambda_xapeu = B_inv_t.dot(np.matrix(c_b).getT())
+    print("lambda xapeu (solução do dual):")
+    print(lambda_xapeu)
+    #Aula 12 - dual 
 
-    aux_troca = I_b[index_sair]
-    I_b[index_sair] = I_n[index_entrar]
-    I_n[index_entrar] = aux_troca
-    return c_xapeu_n, I_b, I_n, x_x_b
-
+    return lambda_xapeu
 
 def resolver_por_primal(A, b, c_t):
     I_b, I_n = obter_particoes_iniciais(A, b)
@@ -397,15 +416,53 @@ def resolver_por_primal(A, b, c_t):
     print("vetor X na sequencia original: ", x_final)
     print("vetor de custos: ", c_t)
     print("valor da solução ótima: ", x_final.dot(c_t))
+
+    B, N = atualizeBN(A, I_b, I_n)
+    
+    c_b, c_n = obter_custos(I_b, I_n, c_t)
+    obter_solucao_dual_dada_primal(B,c_b)
+
     return x_final
 
 
 def resolver_por_dual(A, b, c_t):
-    #I_b, I_n = obter_particoes_iniciais(np.matrix(A).getT(), b)
+    A_tansposta= np.matrix(A).getT()
+    I_b, I_n = obter_particoes_iniciais(A_tansposta, b)
 
-    dual_simplex(np.matrix(A).getT(), c_t, b)  # , I_b, I_n)
+    
 
-    return
+    c_xapeu_n = []
+    x_x_b = []
+
+    x = 0
+    while(x == 0):
+        c_xapeu_n, I_b, I_n, x_x_b = dual_simplex(A_tansposta, c_t, b , I_b, I_n)
+        if c_xapeu_n.min() >= 0:
+            print("i_b: ", I_b)
+            print("i_n: ", I_n)
+            print("x_x_b: ")
+            print(x_x_b)
+            print("fim")
+            x = 1
+        else:
+            print("Ainda não acabou")
+
+    # vetor de variáveis do tamanho da quantidade de variáveis
+    x_final = np.arange(len(A[1]), dtype=float)
+
+    # print(x_final) # preenchido com lixo, por falta de habilidades simples em python.
+    for i in range(len(I_n)):
+        x_final[I_n[i]-1] = 0
+    # print(x_final)
+    for i in range(len(I_b)):
+        x_final[I_b[i]-1] = x_x_b[i].copy()
+    # print(x_final)
+
+    print("vetor X na sequencia original: ", x_final)
+    print("vetor de custos: ", c_t)
+    print("valor da solução ótima: ", x_final.dot(c_t))
+    return x_final
+
 
 
 # c_t = np.array([-2, -1, 0, 0, 0])
@@ -421,10 +478,15 @@ def resolver_por_dual(A, b, c_t):
 #             [3, 2, 0, 0, 1]])
 #b = np.array([4, 6, 18])
 
-c_t = np.array([1, 1, 1])
-A = np.array([[4/5, 2/5, 0],
-             [0, 3/5, 9/9]])
-b = np.array([108, 120])
+#c_t = np.array([1, 1, 1])
+#A = np.array([[4/5, 2/5, 0],
+#             [0, 3/5, 9/9]])
+#b = np.array([108, 120])
+
+c_t = np.array([1, 1, 0, 0])
+A = np.array([[2, 1, -1, 0],
+             [1, 3, 0, -1]])
+b = np.array([4, 3])
 
 print(c_t)
 print(A)
@@ -434,4 +496,4 @@ escrever_problema_dual(A, c_t, b)
 
 print("primal: ", resolver_por_primal(A, b, c_t))
 
-#print("Dual: ", resolver_por_dual(np.matrix(A).getT(), c_t, b))
+#print("Dual: ", resolver_por_dual(A, c_t, b))
