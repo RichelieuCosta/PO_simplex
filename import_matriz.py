67d3234f159d73db87b65ab2628939b4c1118d7b
Richelieu Costa <richelieu.costa@ci.ufpb.br>
2022-05-26 16:28:29 -0300

    adicionando novos recursos, mas precisando de uma correção

--- import_matriz.py
+++ import_matriz.py
@@ -14,14 +14,21 @@ aux = texto.split('=')
 print(aux[0], '=', aux[1])
 
 auxiliar_custos= re.findall(r'.[.\w\d]*', aux[1])
+# append
+# lista_aux = np.arange(n)
+c_t = np.arange((len(auxiliar_custos)-1)/2)
+print("olha o tamanho do vetor: ",len(c_t))
 print(auxiliar_custos)
 for i in range(len(auxiliar_custos)-1):
 
     #print(i)
     if i % 2 == 0: # pegando só os indeces pares.
         #print(i)
-        print(auxiliar_custos[i])
-    
+        print(int(auxiliar_custos[i]))
+        c_t[int(i/2)]=int(auxiliar_custos[i])
+        #print("dividindo i por 2: ", i/2)
+
+print("vetor de custos: ", c_t)
 
 texto = arq.readline()
 print(texto)
@@ -32,6 +39,9 @@ else:
     exit()
 
 texto = arq.readline()
+A = []
+b = []
+aux_index_b = 0
 while(texto != None):
     print()
     texto = texto.split('\n')[0]
@@ -39,20 +49,41 @@ while(texto != None):
     result = re.search(r',', texto)
     # print(result)
     if result != None:
+        print("Restrições: ",b)
         variaveis = texto.split(',')
         print("Variação:")
         print(variaveis)
         exit()
     else:
-        #texto = "5*x1+2*x2<=5"
+        # texto = "5*x1+2*x2<=5"
         aux = re.findall(r'=|<=|>=', texto)
         print(aux)  # ['<=']
         restricao = texto.split(aux[0])
-        print(restricao)
+        print("Restrição: ", restricao)
 
         result = re.findall(r'.[.\w\d]*', restricao[0])
-
-        print(float(fractions.Fraction(restricao[1])))  # 2
         print(result)  # ['5', '*x1', '+2', '*x2']
+        linha = []
+        for i in range(len(result)-1):
+
+            #print(i)
+            if i % 2 == 0: # pegando só os indeces pares.
+                #print(i)
+                linha.append(int(result[i]))
+                print(linha)
+                #print("dividindo i por 2: ", i/2)
+        print("PROBLEMA A SER RESOLVIDO FACILMENTE: RESTRIÇÕES ESTÃO SENDO MONTADAS ERRONEAMENTE QUANDO AS PRIMEIRAS \n variaveis não estão na restrição.")
+        if(len(linha)<len(c_t)):
+            for i in range(len(c_t)-len(linha)):
+                linha.append(0)
+        A.append(linha)
+        b.append(float(fractions.Fraction(restricao[1])))
+        print("matriz A:")
+        print(A)
+        aux_index_b+=1
+        print(float(fractions.Fraction(restricao[1])))  # 2
+        
         print()
     texto = arq.readline()
+print("OLá")
+print(A)
