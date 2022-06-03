982741800ac47a41042fdd10d5259a1ed214fc7a
Richelieu Costa <richelieu.costa@ci.ufpb.br>
2022-06-03 13:09:59 -0300

    comentando prints de depuração

--- principal.py
+++ principal.py
@@ -12,6 +12,12 @@ import fractions
 # 4) determinar valores da solução dual - ok
 #  atualização de nomes das variaveis dual e conclusão. - ok
 # 5) determinar ranges do lado direito - pendente.
+## O que dá pra fazer:
+#1. Variações nas quantidades de recursos;
+#2. Variações nos coeficientes da função objetivo;
+#3. Variações nos coeficientes das atividades;
+#4. Acréscimo de uma nova restrições
+#5. Acréscimo de uma nova variável.
 
 
 def atualizeBN(A, I_b, I_n):
@@ -490,43 +496,45 @@ def carregar_de_arquivo(caminho):
     texto = []  # declaro um vetor
     matriz = []  # declaro um segundo vetor
     texto = arq.readline()  # quebra as linhas do arquivo em vetores
-    print("vetor texto -> ", texto)  # aqui eu mostro
+    # print("vetor texto -> ", texto)  # aqui eu mostro
 
     aux = texto.split('=')
+    print()
     print(aux[0], '=', aux[1])
 
     auxiliar_custos= re.findall(r'.[.\w\d]*', aux[1])
-    print("olha a auxiliar custos: ",auxiliar_custos)
+    # print("olha a auxiliar custos: ",auxiliar_custos)
     # append
     # lista_aux = np.arange(n)
     v_v = []
     num_var = int((len(auxiliar_custos))/2) # SUBTRAIR 1 AQUI O AUXILIAR CUSTOS ?
-    print("Olha a quantidade de variaveis: ", num_var)
+    # print("Olha a quantidade de variaveis: ", num_var)
     c_t = [] #np.arange((len(auxiliar_custos)-1)/2)
     qtd_var_folgas = 0
-    #print("olha o tamanho dos vetores: ",c_t, len(v_v) )
-    print(auxiliar_custos)
+    # print("olha o tamanho dos vetores: ",c_t, len(v_v) )
+    # print(auxiliar_custos)
     for i in range(len(auxiliar_custos)): # SUBTRAIR 1 AQUI O AUXILIAR CUSTOS?
 
         #print(i)
         if i % 2 == 0: # pegando só os indeces pares.
             #print(i)
-            print(int(auxiliar_custos[i]))
+            # print(int(auxiliar_custos[i]))
             c_t.append(int(auxiliar_custos[i]))#[int(i/2)]
             #print("dividindo i por 2: ", i/2)
         else:
             #print("Olha o valor no else", int((i-1)/2))
             v_v.append(auxiliar_custos[i])
 
-    print("vetor de custos: ", c_t)
+    #print("vetor de custos: ", c_t)
     #c_t=np.asarray(c_t)
-    print(c_t)
-    print("vetor de variáveis: ", v_v)
+    #print(c_t)
+    #print("vetor de variáveis: ", v_v)
 
     texto = arq.readline()
     print(texto)
     if texto == "sujeito a:\n":
-        print("sujeito a:")
+        print()
+        #print("sujeito a:")
     else:
         print("FOrmato inválido")
         exit()
@@ -537,46 +545,47 @@ def carregar_de_arquivo(caminho):
     aux_index_b = 0
     igualdades_restricoes = []
     while(texto != None):
-        print()
+       # print()
         texto = texto.split('\n')[0]
         print(texto)
         result = re.search(r',', texto)
         # print(result)
         if result != None:
-            print("Restrições: ",b)
-            print(np.asarray(b))
+            #print("Restrições: ",b)
+            #print(np.asarray(b))
             variaveis = texto.split(',')
-            print("Variação:")
-            print(variaveis)
+            #print("Variação:")
+            #print(variaveis)
             break
         else:
             # texto = "5*x1+2*x2<=5"
             aux = re.findall(r'=|<=|>=', texto)
             igualdades_restricoes.append(aux)
-            print(aux)  # ['<=']
+            #print(aux)  # ['<=']
             if aux[0] == '=':
-                print("entrei pq a restrição é de igualdade")
+                print()
+                #print("entrei pq a restrição é de igualdade")
             else:
                 qtd_var_folgas+=1
                 #print("aumentar tamanho de c_t")
                 c_t.append(0)
 
             restricao = texto.split(aux[0])
-            print("Restrição: ", restricao)
+            #print("Restrição: ", restricao)
 
             result = re.findall(r'.[.\w\d]*', restricao[0])
-            print(result)  # ['5', '*x1', '+2', '*x2']
+            #print(result)  # ['5', '*x1', '+2', '*x2']
             linha = []
             i=1
-            print("olha o meu print aleatório: ", (len(result)-1))
+            #print("olha o meu print aleatório: ", (len(result)-1))
             while (i<=(len(result))):
             ##for i in range(len(result)-1):
-                print("Mostrando variação do indice i ", i)
+                #print("Mostrando variação do indice i ", i)
                 # i += 1
                 # inicio do pensamento atual
 
                 for j in range(int((i-1)/2), (len(v_v)), 1):
-                    print("result[i] :", result[i], "\n v_v[j]:", v_v[j])
+                    #print("result[i] :", result[i], "\n v_v[j]:", v_v[j])
                     if result[i]==v_v[j]:
                         linha.append(int(result[i-1]))
                         break
@@ -591,13 +600,13 @@ def carregar_de_arquivo(caminho):
                     linha.append(0)
             A.append(linha)
             b.append(float(fractions.Fraction(restricao[1])))
-            print("matriz A:")
-            print(A)
-            print(np.asarray(A))
+            #print("matriz A:")
+            #print(A)
+            #print(np.asarray(A))
             aux_index_b+=1
-            print(float(fractions.Fraction(restricao[1])))  # 2
+            #print(float(fractions.Fraction(restricao[1])))  # 2
             
-            print()
+            #print()
         texto = arq.readline()
     print("vamos prosseguir")
     # igualdades_restricoes
@@ -627,7 +636,7 @@ def carregar_de_arquivo(caminho):
                 #print("resolvendo o lado direito")
                 linha.append(0)
             #print("Terminei uma linha", linha)
-    print(np.asarray(A))
+    #print(np.asarray(A))
 
 
 
@@ -659,9 +668,10 @@ A, b, c_t = carregar_de_arquivo('arquivo.txt')
 # Atenção, o parse não aceita frações. para corrigir isso, 
 # modificar a expressão regular que pega os coeficientes
 
-print(c_t)
-print(A)
-print(b)
+#print(c_t)
+#print(np.asarray(A))
+#print(np.matrix(b).getT())
+
 escrever_problema_primal(A, c_t, b)
 escrever_problema_dual(A, c_t, b)
 
